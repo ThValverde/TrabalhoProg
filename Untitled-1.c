7d3bbb3316f488427109a958bf0b745f32fbda96
ThValverde <137911147+ThValverde@users.noreply.github.com>
2023-06-27 21:54:20 -0300

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-int SelecionarClasse(){
-    int Classe;
+typedef struct 
+{
+typedef struct
+{
+
+}
+} personagem;
+
+int SelecionarRaca(){
+    int Raca;
     printf("\n1-Anao\n2-Elfo\n3-Halfling\n4-Humano\n5-Dragonato\n6-Gnomo\n7-Meio-Elfo\n8-Tiefling");
 
-    scanf("%d",&Classe);
+    scanf("%d",&Raca);
 
     return 0;
 }
 
 
 int main(){
-    int Classe;
-    printf("Bem Vindo ao D&D");
-    SelecionarClasse();
+    int Raca;
+    printf("Bem Vindo ao D&D.\nEste programa ira lhe auxiliar na montagem de sua ficha nivel um.\nPara prosseguir, siga as seguintes instrucoes:");
+
+    SelecionarRaca();
 
     return 0;
 }
